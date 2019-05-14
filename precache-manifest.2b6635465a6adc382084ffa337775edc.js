fbc8d37dc0ff970d5d47aa49439912005d82870a
Murillo Kajihara <murillo.kajihara@banky.com.br>
2019-05-13 21:23:03 -0300

    Updates

---
+++ precache-manifest.2b6635465a6adc382084ffa337775edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62acae4e9428b1e069dbadce84e0b74",
+    "url": "/github-list/index.html"
+  },
+  {
+    "revision": "a26259f1bc1dd6d06fc0",
+    "url": "/github-list/static/css/main.dd8ac4bb.chunk.css"
+  },
+  {
+    "revision": "fe6b28079e8cd2bdd4a8",
+    "url": "/github-list/static/js/2.57966a6f.chunk.js"
+  },
+  {
+    "revision": "a26259f1bc1dd6d06fc0",
+    "url": "/github-list/static/js/main.cb7b78d5.chunk.js"
+  },
+  {
+    "revision": "60187282c0da74289bb0",
+    "url": "/github-list/static/js/runtime~main.ea0e0271.js"
+  }
+]);
